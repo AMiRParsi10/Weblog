af871b92255cab010357505d16c30363731bd5e6
Ali <ali8asgari@gmail.com>
2019-09-02 11:14:54 +0430

    v6.6

--- app/http/controllers/admin/adminController.js
+++ app/http/controllers/admin/adminController.js
@@ -10,6 +10,7 @@ class courseController extends controller {
             "uploaded" : 1 ,
             "fileName" : image.originalname ,
             "url" :  `${image.destination}/${image.filename}`.substring(8)
+            // "url" : `https://mag.fitexapp.ir${image.destination}/${image.filename}`.substring(8)
 
         });
     }
