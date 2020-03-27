2db0c4fd841cd5e37a36ecd62b7f9dbbf110bdaf
AMiRParsi10 <46384436+AMiRParsi10@users.noreply.github.com>
2020-03-27 18:17:49 +0430

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".card-body.single-post-content p img").addClass("img-fluid");
+    $(".card-body.single-post-content p img").addClass("img-fluid w-100");
 
 
 });
