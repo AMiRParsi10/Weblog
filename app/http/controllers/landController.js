820ea934bdcc891bd6e74b7e690c8911449b784e
Ali <ali8asgari@gmail.com>
2020-03-27 04:55:25 +0430

    push landingNew:

--- app/http/controllers/landController.js
+++ app/http/controllers/landController.js
@@ -1,18 +1,14 @@
 const controller = require('app/http/controllers/controller');
-
+const Course = require('../../models/course');
 class landController extends controller {
 
 
     async index(req, res) {
-
+        let headers = await Course.find({}).sort({ createdAt : -1 }).skip(0).limit(3).exec();
         const title = 'اپلیکیشن فیتکس';
-        res.render('landing/index', {title});
+        res.render('land/index', {title , headers});
     }
-    async home(req, res) {
 
-        const title = ' فیتکس';
-        res.render('land/index', {title});
-    }
 
 
 }
